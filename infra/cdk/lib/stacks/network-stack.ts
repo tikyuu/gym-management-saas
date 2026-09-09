d35b75ae7e71076c24498647c92e65d48e93536f
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-09 21:54:36 +0900

    feat: Public Subnetのルーティングを追加

--- infra/cdk/lib/stacks/network-stack.ts
+++ infra/cdk/lib/stacks/network-stack.ts
@@ -22,18 +22,43 @@ export class NetworkStack extends Stack {
 
     const internetGateway = new ec2.CfnInternetGateway(this, "InternetGateway");
 
-    new ec2.CfnVPCGatewayAttachment(this, "InternetGatewayAttachment", {
-      internetGatewayId: internetGateway.ref,
+    const internetGatewayAttachment = new ec2.CfnVPCGatewayAttachment(
+      this,
+      "InternetGatewayAttachment",
+      {
+        internetGatewayId: internetGateway.ref,
+        vpcId: vpc.vpcId,
+      },
+    );
+
+    const publicRouteTable = new ec2.CfnRouteTable(this, "PublicRouteTable", {
       vpcId: vpc.vpcId,
     });
 
+    const publicDefaultRoute = new ec2.CfnRoute(this, "PublicDefaultRoute", {
+      destinationCidrBlock: "0.0.0.0/0",
+      gatewayId: internetGateway.ref,
+      routeTableId: publicRouteTable.ref,
+    });
+
+    publicDefaultRoute.addResourceDependency(internetGatewayAttachment);
+
     props.publicSubnets.forEach((subnet) => {
-      new ec2.CfnSubnet(this, subnet.id, {
+      const publicSubnet = new ec2.CfnSubnet(this, subnet.id, {
         availabilityZone: subnet.availabilityZone,
         cidrBlock: subnet.cidrBlock,
         mapPublicIpOnLaunch: false,
         vpcId: vpc.vpcId,
       });
+
+      new ec2.CfnSubnetRouteTableAssociation(
+        this,
+        `${subnet.id}RouteTableAssociation`,
+        {
+          routeTableId: publicRouteTable.ref,
+          subnetId: publicSubnet.ref,
+        },
+      );
     });
   }
 }
