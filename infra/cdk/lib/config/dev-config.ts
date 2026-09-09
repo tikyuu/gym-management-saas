7a089b850137dbba1809d28b7d55e73abf73d282
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-09 21:17:18 +0900

    feat: 開発環境のPublic Subnetを追加

--- infra/cdk/lib/config/dev-config.ts
+++ infra/cdk/lib/config/dev-config.ts
@@ -1,6 +1,17 @@
 export const devConfig = {
   environmentName: "dev",
   region: "ap-northeast-1",
-  availabilityZones: ["ap-northeast-1a", "ap-northeast-1c"],
   vpcCidr: "10.10.0.0/16",
+  publicSubnets: [
+    {
+      id: "PublicSubnet1",
+      availabilityZone: "ap-northeast-1a",
+      cidrBlock: "10.10.1.0/24",
+    },
+    {
+      id: "PublicSubnet2",
+      availabilityZone: "ap-northeast-1c",
+      cidrBlock: "10.10.2.0/24",
+    },
+  ],
 };
