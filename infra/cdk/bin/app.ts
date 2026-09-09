3ce8dd21ba50a59dce29a741ef620e30f81dd3b2
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-09 15:25:52 +0900

    feat: CDKアプリにNetwork Stackを登録

--- infra/cdk/bin/app.ts
+++ infra/cdk/bin/app.ts
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import { NetworkStack } from "../lib/stacks/network-stack";
 
 const app = new cdk.App();
+
+new NetworkStack(app, "DevNetworkStack");
 
 app.synth();
