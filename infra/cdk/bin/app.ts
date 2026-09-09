8dbc3046e3e90912543775003b49acdccb1376e1
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-09 16:26:13 +0900

    feat: 開発環境のVPC本体を定義

--- infra/cdk/bin/app.ts
+++ infra/cdk/bin/app.ts
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
+import { devConfig } from "../lib/config/dev-config";
 import { NetworkStack } from "../lib/stacks/network-stack";
 
 const app = new cdk.App();
 
-new NetworkStack(app, "DevNetworkStack");
+new NetworkStack(app, "DevNetworkStack", {
+  env: {
+    region: devConfig.region,
+  },
+  availabilityZones: devConfig.availabilityZones,
+  vpcCidr: devConfig.vpcCidr,
+});
 
 app.synth();
--- infra/cdk/lib/stacks/network-stack.ts
+++ infra/cdk/lib/stacks/network-stack.ts
@@ -1,8 +1,20 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+interface NetworkStackProps extends StackProps {
+  availabilityZones: string[];
+  vpcCidr: string;
+}
+
 export class NetworkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: NetworkStackProps) {
     super(scope, id, props);
+
+    new ec2.Vpc(this, "Vpc", {
+      availabilityZones: props.availabilityZones,
+      ipAddresses: ec2.IpAddresses.cidr(props.vpcCidr),
+      natGateways: 0,
+      subnetConfiguration: [],
+    });
   }
 }
