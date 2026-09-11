0f392c4de062c602b95bdac72f849198bc9fcf61
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-12 07:19:25 +0900

    refactor: Application Network Constructのpropsを追加

--- infra/cdk/lib/constructs/application-network-construct.ts
+++ infra/cdk/lib/constructs/application-network-construct.ts
@@ -1,7 +1,19 @@
 import { Construct } from "constructs";
+import type { SubnetConfig } from "../types/subnet-config";
+
+interface ApplicationNetworkConstructProps {
+  vpcId: string;
+  resourceNamePrefix: string;
+  subnets: SubnetConfig[];
+  natGatewayId: string;
+}
 
 export class ApplicationNetworkConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: ApplicationNetworkConstructProps,
+  ) {
     super(scope, id);
   }
 }
