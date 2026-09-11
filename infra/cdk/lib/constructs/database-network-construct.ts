f3d5094e0476721bc920f37a7394e8e81bb0063a
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-12 08:55:51 +0900

    refactor: DBルートテーブルをConstructへ移動

--- infra/cdk/lib/constructs/database-network-construct.ts
+++ infra/cdk/lib/constructs/database-network-construct.ts
@@ -1,3 +1,4 @@
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import type { SubnetConfig } from "../types/subnet-config";
 
@@ -8,11 +9,23 @@ interface DatabaseNetworkConstructProps {
 }
 
 export class DatabaseNetworkConstruct extends Construct {
+  public readonly routeTable: ec2.CfnRouteTable;
+
   constructor(
     scope: Construct,
     id: string,
     props: DatabaseNetworkConstructProps,
   ) {
     super(scope, id);
+
+    this.routeTable = new ec2.CfnRouteTable(this, "RouteTable", {
+      tags: [
+        {
+          key: "Name",
+          value: `${props.resourceNamePrefix}-private-database-rt`,
+        },
+      ],
+      vpcId: props.vpcId,
+    });
   }
 }
