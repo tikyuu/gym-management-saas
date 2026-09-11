8566fcdcbd19c9b5ce762d8b109575d4012d81a5
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-11 22:14:27 +0900

    refactor: Private Ingress NetworkをConstructへ移動

--- infra/cdk/lib/constructs/private-ingress-network-construct.ts
+++ infra/cdk/lib/constructs/private-ingress-network-construct.ts
@@ -1,3 +1,4 @@
+import { aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import type { SubnetConfig } from "../types/subnet-config";
 
@@ -8,11 +9,49 @@ interface PrivateIngressNetworkConstructProps {
 }
 
 export class PrivateIngressNetworkConstruct extends Construct {
+  public readonly subnets: ec2.CfnSubnet[];
+
   constructor(
     scope: Construct,
     id: string,
     props: PrivateIngressNetworkConstructProps,
   ) {
     super(scope, id);
+
+    const routeTable = new ec2.CfnRouteTable(this, "RouteTable", {
+      tags: [
+        {
+          key: "Name",
+          value: `${props.resourceNamePrefix}-private-ingress-rt`,
+        },
+      ],
+      vpcId: props.vpcId,
+    });
+
+    this.subnets = props.subnets.map((subnet) => {
+      const privateIngressSubnet = new ec2.CfnSubnet(this, subnet.id, {
+        availabilityZone: subnet.availabilityZone,
+        cidrBlock: subnet.cidrBlock,
+        mapPublicIpOnLaunch: false,
+        tags: [
+          {
+            key: "Name",
+            value: `${props.resourceNamePrefix}-${subnet.name}`,
+          },
+        ],
+        vpcId: props.vpcId,
+      });
+
+      new ec2.CfnSubnetRouteTableAssociation(
+        this,
+        `${subnet.id}RouteTableAssociation`,
+        {
+          routeTableId: routeTable.ref,
+          subnetId: privateIngressSubnet.ref,
+        },
+      );
+
+      return privateIngressSubnet;
+    });
   }
 }
