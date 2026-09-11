d5bb6df81be1e1e6749e93e3a825db54f3ab25d3
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-11 21:30:35 +0900

    refactor: Private Ingress Constructのpropsを追加

--- infra/cdk/lib/constructs/private-ingress-network-construct.ts
+++ infra/cdk/lib/constructs/private-ingress-network-construct.ts
@@ -1,7 +1,18 @@
 import { Construct } from "constructs";
+import type { SubnetConfig } from "../types/subnet-config";
+
+interface PrivateIngressNetworkConstructProps {
+  vpcId: string;
+  resourceNamePrefix: string;
+  subnets: SubnetConfig[];
+}
 
 export class PrivateIngressNetworkConstruct extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: PrivateIngressNetworkConstructProps,
+  ) {
     super(scope, id);
   }
 }
