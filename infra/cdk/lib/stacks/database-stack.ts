38b72a35f521d45dbeb0420fc06fb7325944ef95
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-12 20:44:24 +0900

    refactor: DatabaseStackへネットワーク参照を渡す

--- infra/cdk/lib/stacks/database-stack.ts
+++ infra/cdk/lib/stacks/database-stack.ts
@@ -1,8 +1,13 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, aws_ec2 as ec2 } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+interface DatabaseStackProps extends StackProps {
+  databaseSubnetIds: string[];
+  vpc: ec2.IVpc;
+}
+
 export class DatabaseStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: DatabaseStackProps) {
     super(scope, id, props);
   }
 }
