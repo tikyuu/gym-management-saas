2d257dd3123f539d879e36fc46da8bdfc653a5e6
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-13 15:38:12 +0900

    feat: add staff Cognito user pool

---
+++ infra/cdk/lib/constructs/authentication/staff-authentication-construct.ts
@@ -0,0 +1,50 @@
+import {
+  Duration,
+  RemovalPolicy,
+  aws_cognito as cognito,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+interface StaffAuthenticationConstructProps {
+  removalPolicy: RemovalPolicy;
+  resourceNamePrefix: string;
+  userPoolDeletionProtection: boolean;
+}
+
+export class StaffAuthenticationConstruct extends Construct {
+  public readonly userPool: cognito.IUserPool;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props: StaffAuthenticationConstructProps,
+  ) {
+    super(scope, id);
+
+    this.userPool = new cognito.UserPool(this, "StaffUserPool", {
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      deletionProtection: props.userPoolDeletionProtection,
+      featurePlan: cognito.FeaturePlan.ESSENTIALS,
+      mfa: cognito.Mfa.REQUIRED,
+      mfaSecondFactor: {
+        otp: true,
+        sms: false,
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireSymbols: true,
+        requireUppercase: true,
+        tempPasswordValidity: Duration.days(7),
+      },
+      removalPolicy: props.removalPolicy,
+      selfSignUpEnabled: false,
+      signInAliases: {
+        email: true,
+      },
+      signInCaseSensitive: false,
+      userPoolName: `${props.resourceNamePrefix}-staff-user-pool`,
+    });
+  }
+}
