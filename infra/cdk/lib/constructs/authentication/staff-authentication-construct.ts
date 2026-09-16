18568bdf490e0673794dd034468e14e3f67b731c
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-17 07:52:56 +0900

    スタッフ認証ドメインのRoute 53 Aliasを追加

--- infra/cdk/lib/constructs/authentication/staff-authentication-construct.ts
+++ infra/cdk/lib/constructs/authentication/staff-authentication-construct.ts
@@ -19,6 +19,7 @@ interface StaffAuthenticationConstructProps {
 
 export class StaffAuthenticationConstruct extends Construct {
   public readonly appClient: cognito.IUserPoolClient;
+  public readonly customDomain: cognito.UserPoolDomain;
   public readonly userPool: cognito.IUserPool;
 
   constructor(
@@ -61,7 +62,7 @@ export class StaffAuthenticationConstruct extends Construct {
       managedLoginVersion: cognito.ManagedLoginVersion.NEWER_MANAGED_LOGIN,
     });
 
-    this.userPool.addDomain("StaffCustomDomain", {
+    this.customDomain = this.userPool.addDomain("StaffCustomDomain", {
       customDomain: {
         certificate: props.edgeCertificate,
         domainName: props.staffAuthDomainName,
