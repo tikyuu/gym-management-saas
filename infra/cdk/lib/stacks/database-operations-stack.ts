4c6263703c8c818739e06eb8f80ed63bd919fc76
ken_iwasaki <prayingrun22@rakumail.jp>
2026-09-25 08:05:00 +0900

    feat: DB作業用StackにECR参照を渡す

--- infra/cdk/lib/stacks/database-operations-stack.ts
+++ infra/cdk/lib/stacks/database-operations-stack.ts
@@ -1,8 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, aws_ecr as ecr } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+interface DatabaseOperationsStackProps extends StackProps {
+  repository: ecr.IRepository;
+}
+
 export class DatabaseOperationsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: DatabaseOperationsStackProps) {
     super(scope, id, props);
   }
 }
